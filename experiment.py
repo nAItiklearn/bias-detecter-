783128994bdf532465460e51afb1c0f5c945d7fa
Naitik Sahu <naiitik1526@gmail.com>
2026-08-14 23:56:36 +0530

    added readmme

--- experiment.py
+++ experiment.py
@@ -29,57 +29,75 @@
 
 # ###DATA CLEANING###
 
-#tokenization
-import nltk 
+# #tokenization
+# import nltk 
 
-# nltk.download("punkt")
-from nltk.tokenize import word_tokenize, sent_tokenize
-texts=["im writing this this because free will",
-      "the point of writing _this_ this is to learn #datacleaning #simpe LMAO" ,
-      "YOU are a a a dirty dirty #bitch!!"
-      ]
-
-
-ttext = [word_tokenize(text) for text in texts]
-# print("tokenized docs:", ttext)
-
-#sentence
-# stext = [sent_tokenize(text) for text in texts]
-# print("tokenized sentnce:",stext)
+# # nltk.download("punkt")
+# from nltk.tokenize import word_tokenize, sent_tokenize
+# texts=["im writing this this because free will",
+#       "the point of writing _this_ this is to learn #datacleaning #simpe LMAO" ,
+#       "YOU are a a a dirty dirty #bitch!!"
+#       ]
 
 
-##punctuation removal##
+# ttext = [word_tokenize(text) for text in texts]
+# # print("tokenized docs:", ttext)
 
-import re 
-import string
-regex=re.compile("[%s]"% re.escape(string.punctuation))
-cleaned_raw=[]
+# #sentence
+# # stext = [sent_tokenize(text) for text in texts]
+# # print("tokenized sentnce:",stext)
 
-for review in ttext:
-    new_review=[]
-    for token in review:
-        new_token=regex.sub("", token)
-        if new_token!="":
-            new_review.append(new_token)
+
+# ##punctuation removal##
+
+# import re 
+# import string
+# regex=re.compile("[%s]"% re.escape(string.punctuation))
+# cleaned_raw=[]
+
+# for review in ttext:
+#     new_review=[]
+#     for token in review:
+#         new_token=regex.sub("", token)
+#         if new_token!="":
+#             new_review.append(new_token)
             
     
-    cleaned_raw.append(new_review)
+#     cleaned_raw.append(new_review)
 
-# print(cleaned_raw)
-import nltk
-#remove stopwords
-from nltk.corpus import stopwords
-from collections import Counter
-# nltk.download("stopwords")
-stopwordss=[]
-for doc in cleaned_raw:
-    new_term=[]
-    for word in doc:
-        if not word.lower() in stopwords.words('english'):
-            new_term.append(word)
-    stopwordss.append(new_term)
-frequency = Counter(new_term)
-# print(stopwordss)
-# print(frequency)
-print(frequency["dirty"])
+# # print(cleaned_raw)
+# import nltk
+# #remove stopwords
+# from nltk.corpus import stopwords
+# from collections import Counter
+# # nltk.download("stopwords")
+# stopwordss=[]
+# for doc in cleaned_raw:
+#     new_term=[]
+#     for word in doc:
+#         if not word.lower() in stopwords.words('english'):
+#             new_term.append(word)
+#     stopwordss.append(new_term)
+# frequency = Counter(new_term)
+# # print(stopwordss)
+# # print(frequency)
+# print(frequency["dirty"])
             
+##learning sentiment analysis with vader
+
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as sia
+# sa= sia()
+# sp=sa.polarity_scores("im the worst")
+#{'neg': 0.672, 'neu': 0.328, 'pos': 0.0, 'compound': -0.629} 
+#compund here means a undimensional single value that gives an overview of the overall sentiment of the text , range[1,1]
+import pandas as pd
+from nrclex import NRCLex
+df=pd.read_excel("", sheet_name="sheet1")
+
+def get_nrc_sentiment(text):
+    emotion=NRCLex(str(text))
+    return emotion.raw_emotion_scores
+
+df["nrc_sentiment"]=df["processed_comments"].astype(str).apply(get_nrc_sentiment)
+
+print(df)[["processed_comments", "nrc_sentiments"]]
---
+++ readme.md
@@ -0,0 +1,51 @@
+BIAS DETECTER- "Are you being manupulated?"
+
+Ive build a webpage where someone pasted a news headline or paragraph, and my logic detects things like - 
+emotional language 
+loaded words
+fear/anger/sensationalism
+excessive certainty
+us-vs-them framing
+
+manupilation score:73%
+emotional language:high
+fear based wording:high
+loaded terms: 4
+
+# TECH STACK
+
+## frontend
+ HTML
+ CSS
+ 
+## backend
+ python
+ flask
+
+## data/NLP
+ pandas
+ numpy
+ re - text cleaning
+ collections-word frequency
+
+## visualization
+ matplotlib(generate charts)
+
+
+# FOLDER STRUCTURE
+bias-detector/
+│
+├── app.py
+├── analyzer.py
+├── requirements.txt
+│
+├── data/
+│   └── word_lists.json
+│
+├── templates/
+│   └── index.html
+│
+└── static/
+    ├── style.css
+    └── script.
+    
